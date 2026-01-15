fd910c189648830b1890750f1cbd44b1956cbe8a
bimalab-pixel <orellanabimalab@gmail.com>
2026-01-15 16:07:42 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'venecia-stats-v1';
+
+// Archivos locales para Venecia
+const ASSETS = [
+  './',
+  './index.html',
+  './CENSOLLDM.html',
+  './manifest.json',
+  './logo-venecia-192.png',
+  './logo-venecia-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Instalando Sistema Venecia...');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
